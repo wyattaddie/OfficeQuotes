75f84ab0877eb7b0b66c23b4ee01ee13b0406d8d
root <root@LAPTOP-B0SL779R.localdomain>
2022-11-28 16:36:05 -0500

    new page

--- app.py
+++ app.py
@@ -82,6 +82,14 @@ def quote():
   person = db_session.query(Person).filter_by(approved=True)
   return render_template('all_quotes.html', quotes=quotes, person=person)
 
+@app.route('/people')
+def people():
+
+  people = db_session.query(Person).filter_by(approved=True)
+  context = {'name': 'Jim','age': ':)'}
+  nums = [{'name': 'Jim','age': ':)'},{'name': 'Dwight','age': ';)'}]
+  return render_template('all_people.html', context=context, people=people)
+
 @app.route('/test')
 def test():
   
